68e8d98c7653b4c2d286f7f4724a643cd64003f4
Ethan Zhang <ethanz4@illinois.edu>
2025-08-14 15:52:20 -0700

    Init basic HTTP server.
    This change sets up a basic HTTP server listening on a user-specified port (or 5200, if not specified).

--- server.go
+++ server.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func ShortcutHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("GET %s", r.URL.Path)
+
+	_, err := io.WriteString(w, r.URL.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := flag.Int("port", 5200, "port to listen on.")
+	// db := flag.String("db", "", "database to connect to.")
+	flag.Parse()
+
+	http.HandleFunc("GET /{name}/", ShortcutHandler)
+
+	log.Printf("Listening on port %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
