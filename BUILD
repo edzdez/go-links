c1e32c51e9f3c8e9a9f4d3e2ddfbaf01b44db6d1
Ethan Zhang <ethanz4@illinois.edu>
2025-08-14 17:35:23 -0700

    Open a sqlite db and attach it as middleware.
    We had to downgrade from go 1.25 to go 1.24 due to the lack of pack in the new version.

--- BUILD
+++ BUILD
@@ -5,4 +5,9 @@ go_binary(
     srcs = [
         "server.go"
     ],
+    cgo = True,
+    deps = [
+        "@com_github_adrg_xdg//:xdg",
+        "@com_github_mattn_go_sqlite3//:go-sqlite3"
+    ],
 )
--- server.go
+++ server.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/adrg/xdg"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func withLogging(next http.Handler) http.Handler {
+func WithLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
+	})
+}
+
+func WithDb(db *sql.DB, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "db", db)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
@@ -26,10 +40,45 @@ func ShortcutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func openDb(dbPath string) (*sql.DB, error) {
+	if dbPath == "" {
+		defaultDbPath, err := xdg.DataFile("go-links/shortcuts.sqlite")
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := os.Stat(defaultDbPath); errors.Is(err, os.ErrNotExist) {
+			_, err := os.Create(defaultDbPath)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		dbPath = defaultDbPath
+	}
+
+	log.Printf("Opening db at %s", dbPath)
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
 	port := flag.Int("port", 5200, "port to listen on.")
-	// db := flag.String("db", "", "database to connect to.")
+	dbPath := flag.String("db", "", "database to connect to.")
 	flag.Parse()
+
+	if port == nil || dbPath == nil {
+		log.Fatal("Something went wrong")
+	}
+
+	db, err := openDb(*dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	router := http.NewServeMux()
 
@@ -37,7 +86,7 @@ func main() {
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", *port),
-		Handler: withLogging(router),
+		Handler: WithLogging(WithDb(db, router)),
 	}
 
 	log.Printf("Listening on port %d", *port)
